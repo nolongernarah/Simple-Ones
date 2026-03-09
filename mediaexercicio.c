bb2db852813cc5e6eae1d21b412a76dc37fdb8a2
Arthur Vidal <aragao0697@gmail.com>
2026-03-09 13:02:27 -0300

    Add files via upload

---
+++ mediaexercicio.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main (){
+	
+	float n1,n2,n3,n4;
+	printf("Sua primeira nota e: \n");
+	scanf("%f", &n1);
+	printf("Sua segunda nota e: \n");
+	scanf("%f", &n2);
+	printf("Sua terceira nota nota e: \n");
+	scanf("%f", &n3);
+	printf("Sua quarta nota e: \n");
+	scanf("%f", &n4);
+	 
+	 float media = (n1+n2*2+n3*3+n4*4)/10;
+	 printf("A sua media e %f:\n");
+	 
+	 
+	
+}
