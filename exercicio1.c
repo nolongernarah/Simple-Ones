1f189a803c0938ee698f15665ed0a31fd491ba59
Arthur Vidal <aragao0697@gmail.com>
2026-03-11 14:11:00 -0300

    Add files via upload

---
+++ exercicio1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	int a;
+	
+	printf("Digite um número:\n");
+	scanf("%d", &a);
+	
+	if(a > 0){
+		printf("O número é positivo.");
+		
+	}
+	
+	else{
+		printf("O número é negativo.");
+	}
+	
+}
