97ce1357059bd86e0f27cb81bea4d1ea127f8491
nolongernarah <aragao0697@gmail.com>
2026-05-02 15:10:17 -0300

    Exercícios que estou fazendo

---
+++ foguete.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+    for (int i = 10; i >= 0; i--) {
+
+        printf("%d\n", i);
+
+    }
+
+    printf("Lancamento autorizado!\n");
+    return 0;
+
+}
--- tabuada.c
+++ tabuada.c
@@ -7,7 +7,7 @@ int main(void) {
     printf("Digite um número para ver a tabuada: ");
     scanf("%d", &num);
 
-    for (int i = 1; i <= num; i++) {
+    for (int i = 1; i <= 10; i++) {
         int resultado = num * i;
         printf("%d x %d = %d\n", num, i, resultado);
     }
