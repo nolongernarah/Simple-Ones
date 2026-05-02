a0423d7977b6efdd03cc8cc3e763d5f22cda1898
nolongernarah <aragao0697@gmail.com>
2026-05-02 13:48:01 -0300

    Commit de exercícios que estou fazendo por enquanto

---
+++ senhaacesso.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+    system("chcp 65001 > nul");
+
+    int senhaDigitada = 0;
+    int senhaCorreta = 2026;
+    int tentativas = 1;
+    int limite = 3;
+
+    while (senhaDigitada != senhaCorreta && tentativas <= limite) {
+
+        printf(" Tentativa %d de %d - Digite a sua senha: ", tentativas, limite);
+        scanf("%d", &senhaDigitada);
+
+        if (senhaDigitada == senhaCorreta) {
+            printf("\nAcesso liberado!\n", senhaCorreta);
+        } else {
+            if (tentativas < limite) {
+                printf("Acesso negado! Tente novamente.\n\n ");
+            } else {
+                printf("\nConta bloqueada! Tentativa de limites excedido.\n");
+            }
+        }
+
+        tentativas++;
+    }
+
+    return 0;
+
+}
